c4d3f1b70ccd84df7108d2ea5c8579023246166c
Alex Manak <alexmanak@icloud.com>
2023-02-21 23:29:48 -0800

    keep on keeping on

--- source/vga_plot.c
+++ source/vga_plot.c
@@ -1,5 +1,6 @@
+volatile unsigned *vga_controller = (volatile unsigned *) 0x00004000;
+
 void vga_plot(unsigned x, unsigned y, unsigned colour)
 {
-    volatile unsigned *VGA_controller = (volatile unsigned *) 0x00004000;
-    *VGA_controller = (y << 24) | (x << 16) | colour;
+    *vga_controller = (y << 24) | (x << 16) | colour;
 }
